0985e1e2be0b0f848b5cda259820f66b44b270b4
Danny Serrano <dannyserranoserrano@gmail.com>
2022-04-12 16:07:11 +0200

    Actualizacion_12/04/22

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ mongoose.connect(URL, {}).then(() => {
 })
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/public'))
+    app.use(express.static('client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
